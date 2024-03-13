f4db4adeffcf32bdb028b406b8a93cbb768d7699
georgegebbett <github@georgegebbett.co.uk>
2024-03-13 05:09:58 +0000

    make build_sdks

--- sdk/python/pulumi_stripe/webhook_endpoint.py
+++ sdk/python/pulumi_stripe/webhook_endpoint.py
@@ -285,6 +285,7 @@ class WebhookEndpoint(pulumi.CustomResource):
 
         ## Example Usage
 
+        <!--Start PulumiCodeChooser -->
         ```python
         import pulumi
         import pulumi_stripe as stripe
@@ -297,6 +298,7 @@ class WebhookEndpoint(pulumi.CustomResource):
             ],
             url="https://webhook-url-consumer.com")
         ```
+        <!--End PulumiCodeChooser -->
 
         :param str resource_name: The name of the resource.
         :param pulumi.ResourceOptions opts: Options for the resource.
@@ -321,6 +323,7 @@ class WebhookEndpoint(pulumi.CustomResource):
 
         ## Example Usage
 
+        <!--Start PulumiCodeChooser -->
         ```python
         import pulumi
         import pulumi_stripe as stripe
@@ -333,6 +336,7 @@ class WebhookEndpoint(pulumi.CustomResource):
             ],
             url="https://webhook-url-consumer.com")
         ```
+        <!--End PulumiCodeChooser -->
 
         :param str resource_name: The name of the resource.
         :param WebhookEndpointArgs args: The arguments to use to populate this resource's properties.
