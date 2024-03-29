bcfce862b4f018542ebb004ead0c41d070ce3d24
georgegebbett <github@georgegebbett.co.uk>
2024-03-29 05:10:26 +0000

    make build_sdks

--- sdk/nodejs/promotionCode.ts
+++ sdk/nodejs/promotionCode.ts
@@ -15,6 +15,7 @@ import * as utilities from "./utilities";
  *
  * ## Example Usage
  *
+ * <!--Start PulumiCodeChooser -->
  * ```typescript
  * import * as pulumi from "@pulumi/pulumi";
  * import * as stripe from "pulumi-stripe";
@@ -48,6 +49,7 @@ import * as utilities from "./utilities";
  *     },
  * });
  * ```
+ * <!--End PulumiCodeChooser -->
  */
 export class PromotionCode extends pulumi.CustomResource {
     /**
